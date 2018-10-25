f78fa06198d742c31fa8007a4d71187520424951
be1zi <konrad.belzowski@gmail.com>
2018-10-25 21:55:50 +0200

    Added address model, update cusstomer model

---
+++ src/main/java/com/library/libraryapi/Model/AddressModel.java
@@ -0,0 +1,91 @@
+package com.library.libraryapi.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class AddressModel {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    private long id;
+
+    private String street;
+    private String city;
+    private String zipCode;
+    private String voivodeship;
+    private String houseNumber;
+    private String placeNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getVoivodeship() {
+        return voivodeship;
+    }
+
+    public void setVoivodeship(String voivodeship) {
+        this.voivodeship = voivodeship;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPlaceNumber() {
+        return placeNumber;
+    }
+
+    public void setPlaceNumber(String placeNumber) {
+        this.placeNumber = placeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressModel{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", voivodeship='" + voivodeship + '\'' +
+                ", houseNumber='" + houseNumber + '\'' +
+                ", placeNumber='" + placeNumber + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/library/libraryapi/Model/CustomerModel.java
+++ src/main/java/com/library/libraryapi/Model/CustomerModel.java
@@ -3,6 +3,7 @@ package com.library.libraryapi.Model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -18,6 +19,14 @@ public class CustomerModel {
 
     @NotNull
     private String password;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    @OneToOne
+    private AddressModel address;
 
     public long getId() {
         return id;
@@ -43,12 +52,57 @@ public class CustomerModel {
         this.password = password;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AddressModel getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressModel address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "CustomerModel{" +
                 "id=" + id +
                 ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", address=" + address +
                 '}';
     }
 }
